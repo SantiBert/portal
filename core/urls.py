3e8fc3bae30fe0768c778474007b74cb6c1c4446
SantiBert <santibertero34@gmail.com>
2020-10-13 19:20:21 -0300

    tercer comentario

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ from .views import (ProfileUpdate,
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('administration', AdministrationView.as_view(), name="administration"),
+    path('administration/', AdministrationView.as_view(), name="administration"),
     path('profile/', ProfileUpdate.as_view(), name="profile"),
     path('profile/email/', EmailUpdate.as_view(), name="profile_email"),
     path('profile/name/', NameUpdate.as_view(), name="profile_name"),
